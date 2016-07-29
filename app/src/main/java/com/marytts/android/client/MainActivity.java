008c7c2957ba9bebdeb7bf844887ac7a7197c4c2
fariz_aghayev <fariz.aghayev@gmail.com>
2016-07-29 11:21:09 +0400

    new version

--- app/src/main/java/com/marytts/android/client/MainActivity.java
+++ app/src/main/java/com/marytts/android/client/MainActivity.java
@@ -1,20 +1,51 @@
 package com.marytts.android.client;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.marytts.android.link.MaryLink;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editTtsText = null;
+
+    private TransparentProgressDialog pd;
+    private Handler h;
+    private Runnable r;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,19 +55,34 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         editTtsText = (EditText) findViewById(R.id.editTtsText);
 
-        //initialize MaryTTS and load modules, voice and language modules
-        MaryLink.load(this, this.getResources());
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String text = editTtsText.getText().toString().trim();
-                if(!text.isEmpty()){
-                    MaryLink.getInstance().startButton(text);
+                if (!text.isEmpty()) {
+                    MaryLink.getInstance().startTTS(text);
                 }
             }
         });
+        h = new Handler();
+        pd = new TransparentProgressDialog(this, R.drawable.spinner);
+        r = new Runnable() {
+            @Override
+            public void run() {
+                if (pd.isShowing()) {
+                    pd.dismiss();
+                }
+            }
+        };
+        if (MaryLink.getInstance() == null) {
+            pd.show();
+            h.postDelayed(r, 15000); //wait 15 sec. loading TTS voice models ...
+        }
+        //initialize MaryTTS and load modules, voice and language modules wait 15 sec.
+        MaryLink.load(this);
     }
 
     @Override
@@ -59,5 +105,48 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        h.removeCallbacks(r);
+        if (pd.isShowing()) {
+            pd.dismiss();
+        }
+        super.onDestroy();
+    }
+
+
+    private class TransparentProgressDialog extends Dialog {
+
+        private ImageView iv;
+
+        public TransparentProgressDialog(Context context, int resourceIdOfImage) {
+            super(context, R.style.TransparentProgressDialog);
+            WindowManager.LayoutParams wlmp = getWindow().getAttributes();
+            wlmp.gravity = Gravity.CENTER_HORIZONTAL;
+            getWindow().setAttributes(wlmp);
+            setTitle(null);
+            setCancelable(false);
+            setOnCancelListener(null);
+            LinearLayout layout = new LinearLayout(context);
+            layout.setOrientation(LinearLayout.VERTICAL);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(Toolbar.LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+            iv = new ImageView(context);
+            iv.setImageResource(resourceIdOfImage);
+            layout.addView(iv, params);
+            addContentView(layout, params);
+        }
+
+        @Override
+        public void show() {
+            super.show();
+            RotateAnimation anim = new RotateAnimation(0.0f, 360.0f, Animation.RELATIVE_TO_SELF, .5f, Animation.RELATIVE_TO_SELF, .5f);
+            anim.setInterpolator(new LinearInterpolator());
+            anim.setRepeatCount(Animation.INFINITE);
+            anim.setDuration(3000);
+            iv.setAnimation(anim);
+            iv.startAnimation(anim);
+        }
     }
 }
